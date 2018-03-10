6f8ada15933990141cec71b29c83e75bda0bd610
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-10 11:53:35 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'Mzk5NDU2MDY1NTAwMDg2Mjgw.DYQdUA.MMb6MWnFU4Ygf-3sF9xUdmLP8FI'; // ya wr3 la tns5
+
+exports.PREFIX = '!!';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
